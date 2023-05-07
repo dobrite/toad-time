27e049b86579c0ac1f9dbde35ea20c7c4f8c84e9
Dave O <davidobrite@gmail.com>
2023-05-07 12:28:57 -0700

    chore(seq): update for set_rate fixes

--- src/state/rate.rs
+++ src/state/rate.rs
@@ -4,23 +4,23 @@ use seq::Rate;
 use super::Updatable;
 
 const RATES: [Rate; 17] = [
-    Rate::Div(64),
-    Rate::Div(32),
-    Rate::Div(16),
-    Rate::Div(8),
-    Rate::Div(5),
-    Rate::Div(4),
-    Rate::Div(3),
-    Rate::Div(2),
+    Rate::Div(64.0),
+    Rate::Div(32.0),
+    Rate::Div(16.0),
+    Rate::Div(8.0),
+    Rate::Div(5.0),
+    Rate::Div(4.0),
+    Rate::Div(3.0),
+    Rate::Div(2.0),
     Rate::Unity,
-    Rate::Mult(2),
-    Rate::Mult(3),
-    Rate::Mult(4),
-    Rate::Mult(5),
-    Rate::Mult(8),
-    Rate::Mult(16),
-    Rate::Mult(32),
-    Rate::Mult(64),
+    Rate::Mult(2.0),
+    Rate::Mult(3.0),
+    Rate::Mult(4.0),
+    Rate::Mult(5.0),
+    Rate::Mult(8.0),
+    Rate::Mult(16.0),
+    Rate::Mult(32.0),
+    Rate::Mult(64.0),
 ];
 
 impl Updatable for Rate {
@@ -46,25 +46,26 @@ impl Updatable for Rate {
 pub struct RateString(pub String<3>);
 
 impl From<Rate> for RateString {
+    #[allow(illegal_floating_point_literal_pattern)]
     fn from(val: Rate) -> Self {
         let rate_string = match val {
-            Rate::Div(64) => "/64",
-            Rate::Div(32) => "/32",
-            Rate::Div(16) => "/16",
-            Rate::Div(8) => "/8",
-            Rate::Div(5) => "/5",
-            Rate::Div(4) => "/4",
-            Rate::Div(3) => "/3",
-            Rate::Div(2) => "/2",
+            Rate::Div(64.0) => "/64",
+            Rate::Div(32.0) => "/32",
+            Rate::Div(16.0) => "/16",
+            Rate::Div(8.0) => "/8",
+            Rate::Div(5.0) => "/5",
+            Rate::Div(4.0) => "/4",
+            Rate::Div(3.0) => "/3",
+            Rate::Div(2.0) => "/2",
             Rate::Unity => "x1",
-            Rate::Mult(2) => "x2",
-            Rate::Mult(3) => "x3",
-            Rate::Mult(4) => "x4",
-            Rate::Mult(5) => "x5",
-            Rate::Mult(8) => "x8",
-            Rate::Mult(16) => "x16",
-            Rate::Mult(32) => "x32",
-            Rate::Mult(64) => "x64",
+            Rate::Mult(2.0) => "x2",
+            Rate::Mult(3.0) => "x3",
+            Rate::Mult(4.0) => "x4",
+            Rate::Mult(5.0) => "x5",
+            Rate::Mult(8.0) => "x8",
+            Rate::Mult(16.0) => "x16",
+            Rate::Mult(32.0) => "x32",
+            Rate::Mult(64.0) => "x64",
             _ => unreachable!(),
         };
 
