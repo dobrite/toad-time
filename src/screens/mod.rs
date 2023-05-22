b02b4b086e6671a23ff9c095dfeef88fdacfebb7
Dave O <davidobrite@gmail.com>
2023-05-21 17:30:17 -0700

    chore(style): gate_state to gate_config

--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -45,8 +45,8 @@ impl Screens {
 
     async fn draw_output(&mut self, output: &Output, state: &State, display: &mut Display) {
         display.clear();
-        let gate_state = &state.outputs[output];
-        self.gate.draw(output, gate_state, display);
+        let gate_config = &state.outputs[output];
+        self.gate.draw(output, gate_config, display);
         self.draw_pointer(state.current_element, display);
         display.flush().await;
     }
