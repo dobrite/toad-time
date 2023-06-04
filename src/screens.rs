476f3de07fcfd9cc2a6a0977601e66af5c636f71
Dave O <davidobrite@gmail.com>
2023-06-03 21:29:13 -0700

    chore(display): impl rest of state_change draws

--- src/screens.rs
+++ src/screens.rs
@@ -29,6 +29,31 @@ impl Screens {
     pub fn draw(&mut self, state_change: StateChange, display: &mut Display) {
         match state_change {
             StateChange::Bpm(_) => self.home.draw(&state_change, display),
+            StateChange::Sync(_) => self.home.draw(&state_change, display),
+            StateChange::PlayStatus(_) => self.home.draw(&state_change, display),
+            StateChange::Rate(_, output_type, _) => match output_type {
+                OutputType::Gate => self.gate.draw(&state_change, display),
+                OutputType::Euclid => self.euclid.draw(&state_change, display),
+            },
+            StateChange::Pwm(..) => self.gate.draw(&state_change, display),
+            StateChange::Prob(..) => self.gate.draw(&state_change, display),
+            StateChange::Length(..) => self.euclid.draw(&state_change, display),
+            StateChange::Density(..) => self.euclid.draw(&state_change, display),
+            StateChange::Index(..) => self.euclid.draw(&state_change, display),
+            StateChange::OutputType(ref screen_state) => match screen_state {
+                ScreenState::Output(_, config, _todo) => match config.output_type() {
+                    OutputType::Euclid => self.euclid.draw(&state_change, display),
+                    OutputType::Gate => self.gate.draw(&state_change, display),
+                },
+                _ => unreachable!(),
+            },
+            StateChange::NextElement(ref screen_state, _) => match screen_state {
+                ScreenState::Home(..) => self.home.draw(&state_change, display),
+                ScreenState::Output(_, config, _todo) => match config.output_type() {
+                    OutputType::Euclid => self.euclid.draw(&state_change, display),
+                    OutputType::Gate => self.gate.draw(&state_change, display),
+                },
+            },
             StateChange::NextScreen(ref next_screen) => match next_screen {
                 ScreenState::Home(..) => self.home.draw(&state_change, display),
                 ScreenState::Output(_, config, _todo) => match config.output_type() {
@@ -36,7 +61,6 @@ impl Screens {
                     OutputType::Gate => self.gate.draw(&state_change, display),
                 },
             },
-            _ => {}
         }
     }
 }
